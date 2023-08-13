028f4bc88bc0ac47b3d8e1eb3ffe5fc168f6fd41
Zhayu517 <zhangzy517@gmail.com>
2023-08-13 10:25:24 +0100

    press number after calculation will clear display if display has no operator then calculate does nothing

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -9,22 +9,26 @@ export class CalculatorComponent {
 	displayValue: string = '0';
 	currentExpression: string = '';
 	waitingForNextOperand: boolean = false;
+	pressedEqual: boolean = true;
 
 	onButtonClick(value: string|number) {
-		if (value === '=') {
-			this.calculate();
-		} else if (value === 'DEL') {
+		if (value === 'DEL') {
 			this.deleteLastCharacter();
 		} else {
-			if (this.displayValue === '0') {
-				this.displayValue = value.toString();
-			} else if (this.displayValue === "Error") {
+			if (this.displayValue === "Error") {
 				this.clearDisplay();
 				this.displayValue = value.toString();
+				this.currentExpression = this.displayValue;
 			} else {
-				this.displayValue += value;
+				if (this.pressedEqual) {
+					this.displayValue = value.toString();
+					this.currentExpression = this.displayValue;
+					this.pressedEqual = false;
+				} else {
+					this.displayValue += value;
+					this.currentExpression += value;
+				}
 			}
-			this.currentExpression += value;
 		}
 	}
 
@@ -40,40 +44,57 @@ export class CalculatorComponent {
 	}
 
 	calculate() {
+		if (this.displayValue === "Error" || this.displayValue === "0") {
+			return;
+		}
+
+		if (!(this.displayValue.includes("+") ||
+			this.displayValue.includes("-") ||
+			this.displayValue.includes("*") ||
+			this.displayValue.includes("/"))) {
+			return;
+		}
+
+
 		try {
 			let newRecord = this.displayValue + " = ";
 			this.displayValue = eval(this.currentExpression).toFixed(2).toString();
 			newRecord += this.displayValue;
 
-			var newElement = document.createElement("div");
-			newElement.innerHTML = newRecord;
-			newElement.style.display = "flex";
-			newElement.style.justifyContent = "space-between";
-			newElement.style.fontSize = "20px";
-			
-			var historyContainer = document.getElementById("historyContainer");
-
-			var deleteButton = document.createElement("button");
-			deleteButton.innerHTML = "X";
-			deleteButton.style.color = "green";
-			deleteButton.addEventListener("click", () => {
-				historyContainer?.removeChild(newElement);
-			});
-			
-			if (historyContainer) {
-				newElement.appendChild(deleteButton);
-				historyContainer.appendChild(newElement);
-			}
+			this.addToHistory(newRecord);
 
 			this.currentExpression = this.displayValue;
+			this.pressedEqual = true;
 		} catch (error) {
 			this.displayValue = 'Error';
-			this.currentExpression = '0';
+			this.currentExpression = '';
 		}
 	}
 
 	clearDisplay() {
 		this.displayValue = '0';
 		this.currentExpression = '';
+	}
+
+	addToHistory(newRecord: string) {
+		var newElement = document.createElement("div");
+		newElement.innerHTML = newRecord;
+		newElement.style.display = "flex";
+		newElement.style.justifyContent = "space-between";
+		newElement.style.fontSize = "20px";
+		
+		var historyContainer = document.getElementById("historyContainer");
+
+		var deleteButton = document.createElement("button");
+		deleteButton.innerHTML = "X";
+		deleteButton.style.color = "green";
+		deleteButton.addEventListener("click", () => {
+			historyContainer?.removeChild(newElement);
+		});
+		
+		if (historyContainer) {
+			newElement.appendChild(deleteButton);
+			historyContainer.appendChild(newElement);
+		}
 	}
 }
